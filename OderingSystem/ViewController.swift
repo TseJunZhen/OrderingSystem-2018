c835bbfb4ccff3c2986ce0c854a21c1af1b59070
Tsejunzhen <victorjunzhen@gmail.com>
2018-05-01 11:30:38 +0800

    Initial Commit

---
+++ OderingSystem/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OderingSystem
+//
+//  Created by larvataAndroid on 2018/5/1.
+//  Copyright © 2018年 junzhenIOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
